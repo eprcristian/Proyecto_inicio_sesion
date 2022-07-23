40d0bb2e59c2f6396cbb7cc18e0ffb6327175cc7
eprcristian <eprcristian@gmail.com>
2022-07-23 13:33:30 -0400

    Creacion Usuarios, eliminacion

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    validates :first_name, presence: true, length: { minimum: 2, too_short: ':Nombre debe tener minimo 2 caracteres' } 
+    validates :last_name, presence: true, length: { minimum: 2, too_short: ':Nombre debe tener minimo 2 caracteres' }
+    validates :email_address, presence: true
+    validates :age, presence: true, numericality: { greater_than: 10, less_than: 150, message: 'Please enter a valid age' }
+    
+   
 end
